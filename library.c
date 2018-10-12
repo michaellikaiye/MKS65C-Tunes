4cb7c9123e10f82df00118ed5827f6083a4520f6
Michael Ye <34128025+michaellikaiye@users.noreply.github.com>
2018-10-12 19:44:08 -0400

    ;(

--- library.c
+++ library.c
@@ -2,3 +2,19 @@
 #include<string.h>
 #include<stdlib.h>
 #include"library.h"
+struct song_node *searchS(char *song, char *artist) {
+}
+struct song_node *searchA(char *artist) {
+}
+void print_letters(char letter) {
+}
+void print_songs(char *artist) {
+}
+void print_all() {
+} 
+void print_shuffle() {
+}
+void delete(char *song) {
+}
+void clear() {
+}
